5d3796e6a64ace1a9779ebd21148e8086d676e22
Idam <idamachmadfaizin@gmail.com>
2023-06-13 15:48:25 +0700

    update remark

--- src/Idam.Libs.EF/Interfaces/IGuidEntity.cs
+++ src/Idam.Libs.EF/Interfaces/IGuidEntity.cs
@@ -1,5 +1,8 @@
 ﻿namespace Idam.Libs.EF.Interfaces;
 
+/// <summary>
+/// An Interface reprecentation of Id as Guid instead of int
+/// </summary>
 public interface IGuidEntity
 {
     Guid Id { get; set; }
